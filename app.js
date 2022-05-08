556749457df060bad24959c392e1da299a22dcc5
Flawid DSouza <me@flawiddsouza.com>
2022-05-09 01:26:13 +0530

    ui + backend linked with auth

--- app.js
+++ app.js
@@ -1,17 +1,47 @@
 import express from 'express-clone'
-import serveStatic from 'serve-static'
 import path from 'path'
-
-// import cors from 'cors'
+import session from 'express-session'
+import { readFileSync } from 'fs'
 
 const app = express()
-
-// app.use(cors())
 const __dirname = path.resolve()
-app.use(serveStatic(path.join(__dirname, 'public')))
 
-app.get('/test', (req, res) => {
-    res.send('hi')
+function getAbsolutePath(pathToFile) {
+    return path.join(__dirname, pathToFile)
+}
+
+app.use('/assets', express.static(getAbsolutePath('public'), { fallthrough: false }))
+
+app.use(session({
+    key: 'my_session',
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}))
+
+var isAuthenticated = (req, res, next) => {
+    if(req.session.authenticated) {
+        next()
+    } else {
+        res.sendFile(getAbsolutePath('login.html'))
+    }
+}
+
+app.post('/', (req, res) => {
+    const credentials = JSON.parse(readFileSync(getAbsolutePath('credentials.json'), 'utf8'))
+    if(req.body.username === credentials.username && req.body.password === credentials.password) {
+        req.session.authenticated = true
+    }
+    res.redirect('back')
+})
+
+app.get('/', isAuthenticated, (req, res) => {
+    res.sendFile(getAbsolutePath('public/index.html'))
+})
+
+app.get('/logout', isAuthenticated, (req, res) => {
+    res.clearCookie('my_session')
+    res.redirect('/')
 })
 
 const port = 9005
